69bbeb472421e35f30bf5e5829a3fab0399f0600
wangwei <echeng192@163.com>
2016-04-06 16:15:06 +0800

    1

---
+++ IPM/DAL/IPMContext.cs
@@ -0,0 +1,21 @@
+﻿using IPM.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace IPM.DAL
+{
+    public class IPMContext :DbContext
+    {
+        public IPMContext() :base("IPMContext")
+        {
+
+        }
+        public DbSet<Module> Modules { set; get; }
+        public DbSet<ModuleAction> ModuleActions { set; get; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
