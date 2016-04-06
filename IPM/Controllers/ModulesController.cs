2f8ba7a5eaaec7dc28730536f6a897ac985d52d1
wangwei <echeng192@163.com>
2016-04-06 17:44:06 +0800

    1

---
+++ IPM/Controllers/ModulesController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using IPM.DAL;
+using IPM.Models;
+
+namespace IPM.Controllers
+{
+    public class ModulesController : Controller
+    {
+        private IPMContext db = new IPMContext();
+
+        // GET: Modules
+        public ActionResult Index()
+        {
+            return View(db.Modules.ToList());
+        }
+
+        // GET: Modules/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // GET: Modules/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Modules/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ModuleId,ModuleName,ParentId,ModuleType,Url,Controller,SortOrder,Remark,CreatedBy,CreatedDate")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                module.ModuleId = Guid.NewGuid().ToString();
+                db.Modules.Add(module);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(module);
+        }
+
+        // GET: Modules/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // POST: Modules/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ModuleId,ModuleName,ParentId,ModuleType,Url,Controller,SortOrder,Remark,CreatedBy,CreatedDate")] Module module)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(module).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(module);
+        }
+
+        // GET: Modules/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Module module = db.Modules.Find(id);
+            if (module == null)
+            {
+                return HttpNotFound();
+            }
+            return View(module);
+        }
+
+        // POST: Modules/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Module module = db.Modules.Find(id);
+            db.Modules.Remove(module);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
